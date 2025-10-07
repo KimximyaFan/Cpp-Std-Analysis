753891a27166eec4be8c1daf4e5fd3ad95e8685f
KimximyaFan <107273680+KimximyaFan@users.noreply.github.com>
2025-10-08 01:23:46 +0900

    feat:

--- test.cc
+++ test.cc
@@ -7,29 +7,7 @@ int main()
 {
     /*
         assign
-        front
-        back
-        begin
-        end
-        clear
 
-        emplace
-        emplace back
-        emplace front
-
-        empty
-        erase
-        insert
-        merge
-        pop back
-        pop front
-        push back
-        push front
-
-        remove
-        removeif
-
-        size
         sort
         splice
         reverse
@@ -37,6 +15,4 @@ int main()
         unique
     */
     std::list<int> list(10);
-
-    list.
 }
