7a2767d0bc1c6856faafc5e09b18ce7b1da25a78
KimximyaFan <107273680+KimximyaFan@users.noreply.github.com>
2025-10-12 00:46:29 +0900

    feat:

--- test.cc
+++ test.cc
@@ -1,12 +1,28 @@
 #include <vector>
 #include <iostream>
 #include <list>
+#include <string>
+#include <array>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+/*
 
+priority_queue
+pair
+tuple
+
+unique_pointer
+shared_pointer
+
+*/
 
 int main()
 {
-    /*
-        reverse
-    */
-    std::list<int> list(10);
+
+    std::priority_queue<int> priority_q;
 }
